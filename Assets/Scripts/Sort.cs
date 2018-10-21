1cd1d6723af0f56c7fdf3723af764fb7a1a1605a
SPARK <https://github.com/songjon93>
2018-10-21 02:44:54 -0400

    Retain Drop Down Value
    Static variable to store drop down value

--- Assets/Scripts/Sort.cs
+++ Assets/Scripts/Sort.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class Sort : MonoBehaviour {
     public Dropdown my_dropdown;
     public Initiate init;
+    public static int drop_value = 0;
 
 	// Use this for initialization
 	void Start () {
+        my_dropdown.value = drop_value;
         my_dropdown.onValueChanged.AddListener( delegate {
             HandleSort(my_dropdown);
         });
 	}
 	
     void HandleSort(Dropdown change){
+        drop_value = change.value;
         init.Sort(change.value);
     }
 }
